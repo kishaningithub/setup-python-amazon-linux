b5e57d6df917c6fa1df800994c0b42a24726a006
Kishan B <kishancs46@gmail.com>
2023-11-04 14:28:22 +0530

    build python

--- install-python.sh
+++ install-python.sh
@@ -1,5 +1,27 @@
-function setup_python() {
-    echo "installing python..."
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Follows the guidelines from https://realpython.com/installing-python/#how-to-build-python-from-source-code
+
+function setup_build_prerequisites() {
+  sudo yum groupinstall -y "Development Tools"
+  sudo yum install -y wget gcc openssl-devel bzip2-devel libffi-devel
 }
 
-setup_python
+function setup_python() {
+  python_version="$1"
+  setup_build_prerequisites
+  temp_dir=$(mktmp -d)
+  pushd "${temp_dir}" > /dev/null
+    wget "https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz"
+    tar xvf "Python-${python_version}.tgz"
+    pushd Python-3.8.4 > /dev/null
+      ./configure --enable-optimizations --with-ensurepip=install
+      make -j 8
+      sudo make install
+    popd > /dev/null
+  popd
+}
+
+setup_python "$1"
