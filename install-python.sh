0cdb3ae3de44def239218080612fb891ce5b4bf2
Kishan B <kishancs46@gmail.com>
2023-11-04 23:29:49 +0530

    ensure system does not get affected

--- install-python.sh
+++ install-python.sh
@@ -12,14 +12,24 @@ function setup_build_prerequisites() {
 function setup_python() {
   python_version="$1"
   setup_build_prerequisites
-  temp_dir=$(mktemp -d)
-  pushd "${temp_dir}" > /dev/null
+  mkdir -p ~/.python-versions
+  python_installation_dir=$(~/.python-versions)
+
+  pushd "${python_installation_dir}" > /dev/null
     wget "https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz"
     tar -zxf "Python-${python_version}.tgz"
     pushd "Python-${python_version}" > /dev/null
-      ./configure --enable-optimizations --with-ensurepip=install
+      echo "before configure"
+      pwd
+      ls
+      ./configure --enable-optimizations --prefix=.
       make -j 8
       sudo make install
+      echo "after install"
+      pwd
+      ls
+      pip3=`which pip3`
+      sudo ln -sf $pip3 `dirname $pip3`/pip
     popd > /dev/null
   popd
 }
