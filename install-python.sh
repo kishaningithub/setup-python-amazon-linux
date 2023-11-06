99f815793afdd39695a211f1b5baadc538595435
Kishan B <kishancs46@gmail.com>
2023-11-06 14:52:56 +0530

    Fix shared libraries load error (#2)
    * List files
    
    * Enable shared
    
    * Add name to test workflow
    
    * Add LDFLAGS
    
    * Remove optimizations
    
    * Cleanup
    
    * Add links
    
    * Add enable shared
    
    * Add monitoring for the list of installed binaries
    
    * Setup pre-requisites separately
    
    * Add linking to shared libraries
    
    * Re enable optimizations
    
    * Updating contributing documentation
    
    * Remove --enable-optimizations to speed up builds

--- install-python.sh
+++ install-python.sh
@@ -4,12 +4,6 @@ set -euo pipefail
 
 # Follows the guidelines from https://realpython.com/installing-python/#how-to-build-python-from-source-code
 
-function setup_build_prerequisites() {
-  sudo yum groupinstall -y "Development Tools"
-  sudo yum install -y tar gzip wget
-  sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel
-}
-
 function set_aliases() {
   ln -sf "${python_installation_dir}/bin/python3" "${python_installation_dir}/bin/python"
   ln -sf "${python_installation_dir}/bin/pip3" "${python_installation_dir}/bin/pip"
@@ -19,15 +13,14 @@ function setup_python() {
   python_version="$1"
   python_installation_dir="$2"
 
-  setup_build_prerequisites
-
   mkdir -p "${python_installation_dir}"
   temp_dir=$(mktemp -d)
   pushd "${temp_dir}" >/dev/null
     wget "https://www.python.org/ftp/python/${python_version}/Python-${python_version}.tgz"
     tar -zxf "Python-${python_version}.tgz"
     pushd "Python-${python_version}" >/dev/null
-      ./configure --enable-optimizations --prefix="${python_installation_dir}"
+      # Have not added --enable-optimizations flag because that shoots up the build time by ~5 minutes
+      ./configure --prefix="${python_installation_dir}" --enable-shared
       make -j 8
       make install
     popd >/dev/null
