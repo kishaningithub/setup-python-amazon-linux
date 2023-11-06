853cfb27b57248a12f4ba20a38fb2817900b5be6
Kishan B <kishancs46@gmail.com>
2023-11-06 13:26:02 +0530

    Setup pre-requisites separately

--- install-python.sh
+++ install-python.sh
@@ -4,12 +4,6 @@ set -euo pipefail
 
 # Follows the guidelines from https://realpython.com/installing-python/#how-to-build-python-from-source-code
 
-function setup_build_prerequisites() {
-  sudo yum groupinstall -y "Development Tools"
-  sudo yum install -y tar gzip wget
-  sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel
-}
-
 function set_aliases() {
   ln -sf "${python_installation_dir}/bin/python3" "${python_installation_dir}/bin/python"
   ln -sf "${python_installation_dir}/bin/pip3" "${python_installation_dir}/bin/pip"
@@ -18,8 +12,6 @@ function set_aliases() {
 function setup_python() {
   python_version="$1"
   python_installation_dir="$2"
-
-  setup_build_prerequisites
 
   mkdir -p "${python_installation_dir}"
   temp_dir=$(mktemp -d)
