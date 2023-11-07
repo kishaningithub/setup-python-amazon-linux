13684a5a025bfcdda763acfe226433fba0bc1f7b
Kishan B <kishancs46@gmail.com>
2023-11-07 13:56:01 +0530

    Optimize parallization

--- install-python.sh
+++ install-python.sh
@@ -19,8 +19,9 @@ function setup_python() {
     tar -zxf "Python-${python_version}.tgz"
     pushd "Python-${python_version}" >/dev/null
       # Have not added --enable-optimizations flag because that shoots up the build time by ~5 minutes
+      # Ref for openssl - https://gist.github.com/wizardbeard/d5b641d1fadbaba755823e16eab4dda1#file-python-3-9-slim-dockerfile-L17
       ./configure --prefix="${python_installation_dir}" --enable-shared --with-openssl=/usr/include/openssl
-      make -j 8
+      make -j "$(nproc)"
       make install
     popd >/dev/null
   popd
