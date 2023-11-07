ab2c4bdd35609641472a15f25042f47a184261b2
Kishan B <kishancs46@gmail.com>
2023-11-07 14:48:00 +0530

    Add with-openssl-rpath

--- install-python.sh
+++ install-python.sh
@@ -22,7 +22,7 @@ function setup_python() {
       # - Ref for openssl
       #     - https://gist.github.com/wizardbeard/d5b641d1fadbaba755823e16eab4dda1#file-python-3-9-slim-dockerfile-L17
       #     - https://stackoverflow.com/a/29169795/3316017
-      ./configure --prefix="${python_installation_dir}" --enable-shared --with-ssl
+      ./configure --prefix="${python_installation_dir}" --enable-shared --with-ssl --with-openssl-rpath=auto
       make -j "$(nproc)"
       make install
     popd >/dev/null
